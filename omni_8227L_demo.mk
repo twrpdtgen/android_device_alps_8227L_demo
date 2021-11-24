77a68e3e71820b0aa4db7e3d91c8041bcf23a005
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-24 14:34:43 +0100

    8227L_demo: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: alps Model: 8227L_demo Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_8227L_demo.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from 8227L_demo device
+$(call inherit-product, device/alps/8227L_demo/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := 8227L_demo
+PRODUCT_NAME := omni_8227L_demo
+PRODUCT_BRAND := alps
+PRODUCT_MODEL := 8227L_demo
+PRODUCT_MANUFACTURER := alps
